24746a500546d64f3130534771422fd61bb6e6df
liumapp <liumapp.com@gmail.com>
2018-04-15 22:40:09 +0800

    proxy done

--- proxy.js
+++ proxy.js
@@ -37,5 +37,25 @@ apiServer.listen(port, hostname, () => {
 
 
 const imgServer = http.createServer((req, res) => {
-  const url
+  const url = req.url.split('/img/')[1]
+  const options = {
+    url: url,
+    encoding: null
+  }
+
+  function callback (error , response , body) {
+    if (!error && response.statusCode === 200) {
+      const contentType = response.headers('content-type')
+      res.setHeader('Content-Type', contentType)
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.end(body)
+    }
+  }
+
+  request.get(options , callback)
 })
+
+imgServer.listen(imgPort, hostname, () => {
+  console.log(`图片处理运行在http://${hostname}:${imgPort}`)
+})
+
