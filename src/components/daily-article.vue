242cd600aa33c2a6fe0e5ee09d71269d76050030
liumapp <liumapp.com@gmail.com>
2018-04-16 11:15:14 +0800

    add daily-article vue component

---
+++ src/components/daily-article.vue
@@ -0,0 +1,13 @@
+/**
+ * @file daily-article.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/16/18
+ */
+<template>
+
+</template>
+<script>
+
+</script>
