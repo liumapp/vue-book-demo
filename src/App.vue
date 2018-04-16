4c2a6897f4766a076c3b92cab5aaabe6faced82f
liumapp <liumapp.com@gmail.com>
2018-04-16 09:23:55 +0800

    send get by axios

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app" class="daily">
     <div class="daily-menu-item">
-      <div class="daily-menu-item">每日推荐</div>
-      <div class="daily-menu-item">主题日报</div>
+      <div class="daily-menu-item"
+           :class="{ on: type === 'recommend'}">每日推荐</div>
+      <div class="daily-menu-item"
+           :class="{ on: type === 'daily'}"
+           @click="showThemes = !showThemes">主题日报</div>
+      <ul v-show="showThemes">
+        <li v-for="item in themes">
+          <a :class="{ on: item.id === themeId && type === 'daily'}">{{ item.name }}</a>
+        </li>
+      </ul>
     </div>
     <div class="daily-list">
       <item></item>
@@ -12,8 +20,29 @@
 </template>
 
 <script>
+import $ from './libs/util'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      themes: [],
+      showThemes: false,
+      type: 'recommend',
+      themeId: 0
+    }
+  },
+  methods: {
+    getThemes () {
+      //axios send get
+      $.ajax.get('themes').then(res => {
+        this.themes = res.others;
+      })
+    }
+  },
+  mounted () {
+    //initialize
+    this.getThemes()
+  }
 }
 </script>
 
