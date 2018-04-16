0d9f9f926c4da2666b55f5a775f1a156ebdc4e51
liumapp <liumapp.com@gmail.com>
2018-04-16 08:51:53 +0800

    add basic template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="daily">
+    <div class="daily-menu-item">
+      <div class="daily-menu-item">每日推荐</div>
+      <div class="daily-menu-item">主题日报</div>
+    </div>
+    <div class="daily-list">
+      <item></item>
+    </div>
+    <daily-article></daily-article>
   </div>
 </template>
 
