b2f8fc01e645f319affa07d8ad9ec51209ce8774
liumapp <liumapp.com@gmail.com>
2018-04-16 11:42:47 +0800

    article detail done

--- src/components/daily-article.vue
+++ src/components/daily-article.vue
@@ -6,8 +6,39 @@
  * @date 4/16/18
  */
 <template>
-
+  <div class="daily-article">
+    <div class="daily-article-title">{{ data.title }}</div>
+    <div class="daily-article-content" v-html="data.body"></div>
+  </div>
 </template>
 <script>
-
+import $ from '../libs/util'
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    getArticle () {
+      $.ajax.get('news/' + this.id).then(res => {
+        res.body = res.body.replace(/src="http/g, 'src="' + $.imgPath + 'http')
+        res.body = res.body.replace(/src="https/g,'src="' + $.imgPath + 'https')
+        this.data = res
+        window.scrollTo(0, 0)
+      })
+    }
+  },
+  watch: {
+    id (val) {
+      if (val) this.getArticle()
+    }
+  }
+}
 </script>
